5e67eea93cea217c8abb05d01c71a2f90c635901
Zeina Nossier <zosman@noon.com>
2025-10-21 17:16:55 +0300

    Initial working setup with Vite + Tailwind v3

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-slate-900 text-white">
+      <h1 className="text-5xl font-bold text-pink-500">Tailwind v3 Working!</h1>
+      <p className="text-green-400 mt-4">If this is colored, we’re done 🎉</p>
+    </div>
+  );
+}
